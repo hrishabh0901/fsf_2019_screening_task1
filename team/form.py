9c739f9312ca8f17fcd8f59edef34e1095737165
hrishabh <hrishabh@gmail.com>
2019-03-31 12:33:11 +0530

    Final Commit

--- team/form.py
+++ team/form.py
@@ -10,21 +10,22 @@ try:
         s2 = set([x for t in Team.objects.all() for x in t.members])
         choices = [(x, x) for x in list(s1 - s2) if x != user.username]
         return choices
+except:
+    pass
 
-
-    class TeamForm(forms.ModelForm):
-
+class TeamForm(forms.ModelForm):
+    try:
         s1 = set([x.username for x in User.objects.all()])
         s2 = set([x for t in Team.objects.all() for x in t.members])
         choices = [(x, x) for x in list(s1 - s2)]
         members = forms.MultipleChoiceField(label="Select Members",choices=choices,widget=forms.CheckboxSelectMultiple,required=False)
+    except:
+        pass
+    def __init__(self,user,*args, **kwargs):
+        super(TeamForm,self).__init__(*args, **kwargs)
+        self.fields['members'].choices = get_menu_choices(user)
 
-        def __init__(self,user,*args, **kwargs):
-            super(TeamForm,self).__init__(*args, **kwargs)
-            self.fields['members'].choices = get_menu_choices(user)
+    class Meta:
+        model = Team
+        fields = ('team_name', 'members')
 
-        class Meta:
-            model = Team
-            fields = ('team_name', 'members')
-except:
-    pass
